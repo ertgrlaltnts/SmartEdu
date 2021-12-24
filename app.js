dc0a2a6a7fb3d30806b557d941ced72a181e83b5
ertgrlaltnts <69998085+ertgrlaltnts@users.noreply.github.com>
2021-12-24 18:55:59 +0300

    V2

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const ejs = require('ejs');
+const res = require('express/lib/response');
 const app = express();
 
-app.set('view ')
+app.set('view engine' , 'ejs');
 
+
+//MIDDLEWARE
+
+app.use(express.static('public'));
 
 app.get('/' , (req,res)=>{
-    res.status(200).send('INDEX SAYFASI');
+    res.status(200).render('index' , {
+        page_name : 'index',
+    });
 })
+
+app.get('/about' , (req,res)=>{
+    res.status(200).render('about', {
+        page_name : 'about',
+    });
+})
+
 
 
 
